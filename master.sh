6ff125325220fb83f30a6cbbc5735bd80844947f
Tige Phillips <tigelane@mac.com>
2017-12-14 17:48:45 -0800

    blah

--- master.sh
+++ master.sh
@@ -1,7 +1,9 @@
 echo export Master_TIGE_VAR_SER=@@{Master.TIGE_VAR_SER}@@ >> .bash_profile
-echo export IGNW_InstallURL=@@{IGNW_InstallURL}@@ >> .bash_profile
+echo IGNW_InstallURL=@@{IGNW_InstallURL}@@ >> .bash_profile
 echo export IGNW_1=@@{IGNW_1}@@ >> .bash_profile
 echo export Master_ipaddress=@@{Master.ipaddress}@@ >> .bash_profile
+echo export IGNW_InstallURL >> .bash_profile
+
 
 curl @@{IGNW_InstallURL}@@ > setup_server.sh
 chmod 766 setup_server.sh
--- setup_server.sh
+++ setup_server.sh
@@ -4,9 +4,9 @@
 sudo yum -y update
 sudo yum install -y git
 
-sudo echo $IGNW_InstallURL > script.log
-sudo echo $Master_ipaddress >> script.log
-sudo bash -c "echo $IGNW_InstallURL >> script.log"
+echo $IGNW_InstallURL > script.log
+echo $Master_ipaddress >> script.log
+echo $IGNW_InstallURL >> script.log
 
 cd /opt
 sudo wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151-linux-x64.tar.gz"
