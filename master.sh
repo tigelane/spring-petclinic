e60dd6dd27cba035aab02ca3eda6bc93a7e34b6c
Tige Phillips <tigelane@mac.com>
2017-12-15 14:41:33 -0800

    refactor to build a new script and use some new vars / macros

--- master.sh
+++ master.sh
@@ -1,10 +1,7 @@
-echo export Master_TIGE_VAR_SER=@@{Master.TIGE_VAR_SER}@@ >> .bash_profile
-echo IGNW_InstallURL=@@{IGNW_InstallURL}@@ >> .bash_profile
-echo export IGNW_1=@@{IGNW_1}@@ >> .bash_profile
-echo export Master_ipaddress=@@{Master.ipaddress}@@ >> .bash_profile
-echo export IGNW_InstallURL >> .bash_profile
-
+echo export IGNW_1=@@{IGNW_1}@@ > envv.sh
+echo export IGNW_InstallURL=@@{IGNW_InstallURL}@@ >> envv.sh
+echo export MasterAddress=@@{Master.address}@@ >> envv.sh
 
 curl @@{IGNW_InstallURL}@@ > setup_server.sh
 chmod 766 setup_server.sh
-sudo ./setup_server.sh
+./setup_server.sh
--- setup_server.sh
+++ setup_server.sh
@@ -4,9 +4,8 @@
 sudo yum -y update
 sudo yum install -y git
 
-echo $IGNW_InstallURL > script.log
-echo $Master_ipaddress >> script.log
-echo $IGNW_InstallURL >> script.log
+source envv.sh
+echo $IGNW_InstallURL > setup.log
 
 cd /opt
 sudo wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151-linux-x64.tar.gz"
@@ -20,7 +19,7 @@ sudo alternatives --set jar /opt/jdk1.8.0_151/bin/jar
 sudo alternatives --set javac /opt/jdk1.8.0_151/bin/javac
 
 cd /opt
-git clone https://github.com/tigelane/spring-petclinic.git
+sudo git clone https://github.com/tigelane/spring-petclinic.git
 cd spring-petclinic
 sudo ./mvnw spring-boot:run > pet.log 2>&1 &
 sleep 10
