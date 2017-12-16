8cd64b3dae8335c71c86b1b713dee078cb279c22
Tige Phillips <tigelane@mac.com>
2017-12-16 10:22:57 -0800

    Corrected var spelling

--- master.sh
+++ master.sh
@@ -1,4 +1,4 @@
-echo export IGNW_INSTALLURL=@@{IGNW_InstallURL}@@ > envv.sh
+echo export IGNW_INSTALLURL=@@{IGNW_INSTALLURL}@@ > envv.sh
 echo export IGNW_BRANCH=@@{IGNW_BRANCH}@@ >> envv.sh
 
 curl @@{IGNW_INSTALLURL}@@ > setup_server.sh
