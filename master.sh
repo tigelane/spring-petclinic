25a614a71e976a419b60b6f490d53c75ceed2462
Tige Phillips <tigelane@mac.com>
2017-12-16 09:28:37 -0800

    Updates

--- master.sh
+++ master.sh
@@ -1,7 +1,5 @@
-echo export IGNW_1=@@{IGNW_1}@@ > envv.sh
-echo export IGNW_InstallURL=@@{IGNW_InstallURL}@@ >> envv.sh
-echo export MasterAddress=@@{Master.address}@@ >> envv.sh
+echo export IGNW_INSTALLURL=@@{IGNW_InstallURL}@@ > envv.sh
 
-curl @@{IGNW_InstallURL}@@ > setup_server.sh
+curl @@{IGNW_INSTALLURL}@@ > setup_server.sh
 chmod 766 setup_server.sh
 ./setup_server.sh
--- setup_server.sh
+++ setup_server.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 # https://raw.githubusercontent.com/tigelane/spring-petclinic/master/setup_server.sh
+source envv.sh
+echo "--Executing from $IGNW_INSTALLURL --" > setup.log
+
+echo "--Updating yum--" > setup.log
 sudo yum -y update
+echo "--Installing git--" > setup.log
 sudo yum install -y git
 
-source envv.sh
-echo $IGNW_InstallURL > setup.log
-
+echo "--Installing Java 1.8--" > setup.log
 cd /opt
 sudo wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151-linux-x64.tar.gz"
 sudo tar xzf jdk-8u151-linux-x64.tar.gz
@@ -18,9 +21,8 @@ sudo alternatives --install /usr/bin/javac javac /opt/jdk1.8.0_151/bin/javac 2
 sudo alternatives --set jar /opt/jdk1.8.0_151/bin/jar
 sudo alternatives --set javac /opt/jdk1.8.0_151/bin/javac
 
-cd /opt
-sudo git clone https://github.com/tigelane/spring-petclinic.git
+cd ~/
+git clone https://github.com/tigelane/spring-petclinic.git
 cd spring-petclinic
-sudo ./mvnw spring-boot:run > pet.log 2>&1 &
-sleep 10
-ls
+sudo nohup ./mvnw spring-boot:run 2>&1 &
+sleep 30
